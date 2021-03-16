12c30fc744be94a7f49e70cdae1e75a14acef69a
TungTang <76823119+clpolcurs@users.noreply.github.com>
2021-03-16 12:52:42 +0700

    modified

--- check_lottery_results.py
+++ check_lottery_results.py
@@ -1,23 +1,11 @@
 __doc__ = """
-Viết một script kiểm tra xem các số argument đầu vào có trúng lô không
-(2 số cuối trùng với một giải nào đó). Nếu không có argument nào thì print
-ra tất cả các giải từ đặc biệt -> giải 7.
+Make a script to check whether arguments match the someday lottery's result (only the last 2 digits of the resuls). If there's no argument input, print the results of that day.
 
-Lấy kết quả từ ``ketqua.net``.
+Crawl results from `ketqua.net`.
 
-Dạng của câu lệnh::
+Command prompt:
 
-  ketqua.py [NUMBER1] [NUMBER2] [...]
-
-Các thư viện:
-
-- requests
-- requests_html hay beautifulsoup4 [tuỳ chọn]
-- argparse hay sys.argv
-
-Gợi ý:
-
-- ``nargs`` https://docs.python.org/2/library/argparse.html
+  check_lottery_results.py [NUMBER1] [NUMBER2] [...]
 """
 
 import requests
@@ -32,19 +20,24 @@ date = today.strftime('%m/%d/%y')
 def get_prizes():
     ses = requests.session()
     req = ses.get('http://ketqua.net/')
-    soup = bs4.BeautifulSoup(req.text, 'lxml')                                                 #using "lxml" for speed issue instead of "html.parser"
-    list_of_ids = [tag['id'] for tag in soup.select('div[id]') if tag['id'].startswith('rs')]  #get all the ids from soup which start with 'rs' including rs_0_0 that stands for no prize
+    # using "lxml" for speed issue instead of "html.parser"
+    soup = bs4.BeautifulSoup(req.text, 'lxml')
+    # get all the ids from soup which start with 'rs' including rs_0_0 that stands for no prize
+    list_of_ids = [tag['id'] for tag in soup.select(
+        'div[id]') if tag['id'].startswith('rs')]
     results = []
     for id in list_of_ids:
         result = soup.find(attrs={'id': '{}'.format(id)}).text
         results.append(result)
-    
-    return results[1:]                                                                         #get rid of rs_0_0
 
-def solve(*args):    
+    return results[1:]  # get rid of rs_0_0
+
+
+def solve(*args):
     count = 0
     for number in args:
-        if number[-2:] in [result[-2:] for result in get_prizes()]:                            # compare to the list of 2 last digits of prizes
+        # compare to the list of 2 last digits of prizes
+        if number[-2:] in [result[-2:] for result in get_prizes()]:
             count += 1
             print('Congrat! {} is your lucky number!'.format(number))
     if count == 0:
